801218f761b9b9932e4f75880f4497f6d73ae74f
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-07 14:05:06 +0600

    array prectise

--- main.js
+++ main.js
@@ -4,20 +4,3 @@ const imageUrls = [
   "https://via.placeholder.com/600x300/666666/ffffff?text=Slide+3"
 ];
 
-let currentIndex = 0;
-
-function change(index){
-  currentIndex += index;
-  if(currentIndex < 0){
-    currentIndex = imageUrls.length - 1;
-  }else if(currentIndex >= imageUrls.length){
-    currentIndex = 0;
-  }
-  showImage()
-}
-
-function showImage(){
-  const images = document.querySelector('.images');
-  images.src = imageUrls[currentIndex];
-}
-showImage()
