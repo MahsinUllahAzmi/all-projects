71ccb715fafe8b7bbddab3bb102e9d0dc055245e
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-11 01:57:56 +0600

    Lightbox start

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-const modalImage = document.querySelector('.modalImage');
+const modalImage = document.querySelectorAll('.modalImage');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
+const content = document.querySelector('.content');
 const popupImage = document.querySelector('.popupImage');
 const popupTitle = document.querySelector('.popupTitle');
-const content = document.querySelector('.content');
 
-
-modalImage.addEventListener('click', function(){
-    modal.classList.remove('hidden');
-    popupImage.src = this.src;
-    popupTitle.innerHTML = this.alt;
-    popupImage.classList.add('w-[600px]' , 'h-[400px]')
-    content.classList.add('scale-[1]')
-    close.addEventListener('click', () =>{
-        modal.classList.add('hidden');
+modalImage.forEach(modalImage => {
+    modalImage.addEventListener('click', function() {
+        modal.classList.remove('hidden');
+        popupImage.src = this.src;
+        popupTitle.innerHTML = this.alt;
+        close.addEventListener('click', () => {
+            modal.classList.add('hidden');
+        });
     });
 });
 
-// popupTitle.innerHTML = this.alt;
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        modal.classList.add('hidden');
+        console.log('ok')
+    }
+  });
