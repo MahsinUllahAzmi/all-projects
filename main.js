848220a040609f710932d0e80299df1b46c2dc66
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-14 01:26:55 +0600

    Tab Gallery done

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
-const tabData = [
-    {
-        title: 'London',
-        descrip: 'London is the capital city of England.'
-    },
-    {
-        title: 'Paris',
-        descrip: 'Paris is the capital of France.'
-    },
-    {
-        title: 'Tokyo',
-        descrip: 'Tokyo is the capital of Japan.'
-    }
-]
+const tabImage = document.querySelectorAll('.tabImage');
+const imgShow = document.querySelector('.imgShow');
+const caption = document.querySelector('.caption');
+const content = document.querySelector('.content');
+const closeer = document.querySelector('.closeer');
 
-const tabBtn = document.querySelectorAll('.tabBtn');
-const title = document.querySelector('.title');
-const description = document.querySelector('.description');
-
-tabBtn.forEach((item , index) => {
-    item.addEventListener('click', () =>{
-        title.innerHTML = tabData[index].title;
-        description.innerHTML = tabData[index].descrip;
-        tabBtn.forEach((item) => {
-            item.classList.remove('bg-gray-200');
-        })
-        tabBtn[index].classList.add('bg-gray-200');
-    })
-    title.innerHTML = tabData[0].title;
-    description.innerHTML = tabData[0].descrip;
+tabImage.forEach((item , index) => {
+    item.addEventListener('click', function(){
+        imgShow.src = item.src;
+        caption.innerHTML = item.alt;
+        content.classList.remove('hidden');
+        closeer.addEventListener('click' , function(){
+            content.classList.add('hidden');
+        });
+        tabImage.forEach(item => {
+            item.classList.add('opacity-[0.7]');
+        });
+        tabImage[index].classList.remove('opacity-[0.7]');
+    });
+    tabImage[index].classList.add('opacity-[0.7]');
+    imgShow.src = tabImage[0].src;
+    caption.innerHTML = tabImage[0].alt;
 });
+
