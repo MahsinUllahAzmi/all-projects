d2fc8bfd97155767795591e904c03c273b691445
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-14 19:41:48 +0600

    Image Grid

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-const tabImage = document.querySelectorAll('.tabImage');
-const imgShow = document.querySelector('.imgShow');
-const caption = document.querySelector('.caption');
-const content = document.querySelector('.content');
-const closeer = document.querySelector('.closeer');
+const grid_content = document.querySelector('.grid_content');
+const btn = document.querySelectorAll('.btn');
 
-tabImage.forEach((item , index) => {
-    item.addEventListener('click', function(){
-        imgShow.src = item.src;
-        caption.innerHTML = item.alt;
-        content.classList.remove('hidden');
-        closeer.addEventListener('click' , function(){
-            content.classList.add('hidden');
-        });
-        tabImage.forEach(item => {
-            item.classList.add('opacity-[0.7]');
-        });
-        tabImage[index].classList.remove('opacity-[0.7]');
+btn.forEach(item => {
+    item.addEventListener('click', () => {
+        grid_content.classList.add('grid-cols-2')
     });
-    tabImage[index].classList.add('opacity-[0.7]');
-    imgShow.src = tabImage[0].src;
-    caption.innerHTML = tabImage[0].alt;
 });
-
