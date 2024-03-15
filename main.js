6c92ce756e6f8e48ceda2e7a97b15b60978e3468
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-15 18:38:10 +0600

    Image Grid done

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const grid_content = document.querySelector('.grid_content');
 const btn = document.querySelectorAll('.btn');
 
-btn.forEach(item => {
+btn.forEach((item, index) => {
     item.addEventListener('click', () => {
-        grid_content.classList.add('grid-cols-2')
+        if(index < 1){
+            grid_content.classList.add('grid-cols-1');
+            grid_content.classList.remove('grid-cols-2');
+            grid_content.classList.remove('grid-cols-3');
+        }
+        else if(index < 2){
+            grid_content.classList.add('grid-cols-2');
+            grid_content.classList.remove('grid-cols-3');
+            grid_content.classList.remove('grid-cols-1');
+            grid_content.classList.remove('grid-cols-4');
+        }
+        else if(index < 3){
+            grid_content.classList.add('grid-cols-3');
+            grid_content.classList.remove('grid-cols-4');
+        }
+        else{
+            grid_content.classList.add('grid-cols-4');
+            // grid_content.classList.remove('grid-cols-3');
+        }
     });
 });
