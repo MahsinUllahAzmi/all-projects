89e4c3601f23fc1603c8b3fda65ae2771ce83b18
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-16 19:06:38 +0600

    JavaScript Progress Bar done

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-const grid_content = document.querySelector('.grid_content');
-const btn = document.querySelectorAll('.btn');
-
-btn.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        if(index < 1){
-            grid_content.classList.add('grid-cols-1');
-            grid_content.classList.remove('grid-cols-2');
-            grid_content.classList.remove('grid-cols-3');
+const value = document.querySelector('.value');
+let currentIndex = 0;
+function runBar(){
+    if(currentIndex == 0){
+        currentIndex = 1;
+        const bar = document.querySelector('.bar');
+        width = 1;
+        let timeMenage = setInterval(frame , 10);
+        function frame(){
+            if(width >= 100){
+                clearInterval(timeMenage);
+                currentIndex = 0;
+            }else{
+                width++;
+                bar.style.width = width + '%';
+            }
+            value.innerHTML = width + '%';
         }
-        else if(index < 2){
-            grid_content.classList.add('grid-cols-2');
-            grid_content.classList.remove('grid-cols-3');
-            grid_content.classList.remove('grid-cols-1');
-            grid_content.classList.remove('grid-cols-4');
-        }
-        else if(index < 3){
-            grid_content.classList.add('grid-cols-3');
-            grid_content.classList.remove('grid-cols-4');
-        }
-        else{
-            grid_content.classList.add('grid-cols-4');
-        }
-    });
-});
+    }
+  }
