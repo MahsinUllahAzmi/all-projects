dd8e332d97510fe48c99cddf578e1cb9b384378b
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-16 23:02:48 +0600

    Basic Array Operations (Methods) done

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-const value = document.querySelector('.value');
-let currentIndex = 0;
-function runBar(){
-    if(currentIndex == 0){
-        currentIndex = 1;
-        const bar = document.querySelector('.bar');
-        width = 1;
-        let timeMenage = setInterval(frame , 10);
-        function frame(){
-            if(width >= 100){
-                clearInterval(timeMenage);
-                currentIndex = 0;
-            }else{
-                width++;
-                bar.style.width = width + '%';
-            }
-            value.innerHTML = width + '%';
-        }
-    }
-  }
+//  Basic Array Operations (Methods)
+
+// Add Elements
+const friends = ['Dhaka', 'Mymensingh', 'Khulna'];
+const newLength = friends.push('Rangpur')
+console.log(friends)
+console.log(newLength)
+
+friends.unshift('Jamalpur')
+console.log(friends)
+
+
+// Remove Elements
+ const popped = friends.pop() //last
+console.log(popped)
+console.log(friends)
+
+friends.shift() //first
+console.log(friends)
+
+console.log(friends.indexOf('Mymensingh')) //1
+console.log(friends.indexOf('dhaka')) //-1
+
+friends.push(25)
+console.log(friends.includes('Mymensingh')) //true
+console.log(friends.includes('dhaka')) //false
+console.log(friends.includes(25)) //true
+console.log(friends.includes('25')) //false
+
+if(friends.includes('Mymensingh')){
+    console.log('Our district in Mymensingh')
+}
