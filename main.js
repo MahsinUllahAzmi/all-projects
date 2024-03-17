d5fdbb1d6c403ced3040758167693a9abb1529f6
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-17 11:59:09 +0600

    Introduction to Objects

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-// CHALLENGE #2
-// Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+const MahsinArray = [
+    'Mahsin',
+    'Mymensingh',
+    2024-2023,
+    'WebDeveloper',
+    ['Karim','Kamal','Poshi','Sabrin']
+];
 
-// Your tasks:
-
-// Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-
-// And now let's use arrays! So, create an array called bills containing the test data below.
-
-// Create an array called tips containing the tip value for each bill, calculated from the function you created before.
-
-// BONUS: Create an array totals containing the total values, so the bill + tip.
-
-
-const calcTip = function(bill){
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; 
+const mahsin = {
+    firstName: 'Mahsin',
+    lastName: 'Ullah',
+    BirthDate: 2024-2004,
+    jobs: 'WebDeveloper',
+    friends: ['Karim','Kamal','Poshi','Sabrin']
 }
 
-// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-
-const bill = [125, 555, 44];
-const tips = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
-const totals = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
-console.log(bill, tips, totals);
+console.log(mahsin)
