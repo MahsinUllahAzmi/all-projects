f86a9ff723f9b456b1c8fb4afd8dac987321d3a7
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-22 12:47:04 +0600

    javaScript prograsse bar done

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function runBar(){
-    console.log('mahsin');
-   let currentIndex = 0;
+    let currentIndex = 0;
+
     if(currentIndex == 0){
-        
+        currentIndex = 1;
+        setInterval(() => {
+            let width = currentIndex++;  
+            if(width <= 100){
+                console.log(width)
+                const bar = document.querySelector('.bar');
+                const value = document.querySelector('.value');
+                bar.style.width = width + '%';
+                value.innerHTML = `${width}%`
+            }
+        } , 10)
     }
 }
