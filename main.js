376e417ab9b8e614915470de267ebc3b5ff90a5d
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-22 15:09:09 +0600

    Animated Menu Icon done

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-function runBar(){
-    let currentIndex = 0;
-
-    if(currentIndex == 0){
-        currentIndex = 1;
-        setInterval(() => {
-            let width = currentIndex++;  
-            if(width <= 100){
-                console.log(width)
-                const bar = document.querySelector('.bar');
-                const value = document.querySelector('.value');
-                bar.style.width = width + '%';
-                value.innerHTML = `${width}%`
-            }
-        } , 10)
-    }
+function changeBar(x){
+    const bar1 = document.querySelector('.bar-1')
+    const bar2 = document.querySelector('.bar-2')
+    const bar3 = document.querySelector('.bar-3')
+    bar1.classList.toggle('activeBar1')
+    bar2.classList.toggle('opacity-0')
+    bar3.classList.toggle('activeBar3')
 }
