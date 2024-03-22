421abb81a135a62c591c050a33dc2ffc25d7ce2a
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-22 10:33:37 +0600

    javaScript progress bar start

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
-const mahsin = {
-    firstName: 'Mahsin',
-    lastName: 'Ullah',
-    BirthYears: 2004,
-    jobs: 'WebDeveloper',
-    friends: ['Karim','Kamal','Poshi','Sabrin'],
-    hasDiverLi: true,
-    // calcAge: function(BirthYears){
-    //     return 2024 - BirthYears;
-    // }
-
-    // calcAge: function(){
-    //     return 2024 - this.BirthYears;
-    // }
-
-
-    calcAge: function(){
-        this.ages = 2024 - this.BirthYears;
-        return this.ages;
-    },
-    getSummery: function(){
-        return `${this.firstName} is a ${this.calcAge()} years old ${this.jobs}, and he has ${this.hasDiverLi ? 'a' : 'no'} , driver is license..`
+function runBar(){
+    console.log('mahsin');
+   let currentIndex = 0;
+    if(currentIndex == 0){
+        
     }
-};   
-
-console.log(mahsin.calcAge())
-console.log(mahsin.getSummery())
-// console.log(mahsin.ages)
-// console.log(mahsin.ages)
-// console.log(mahsin.ages)
-// console.log(mahsin.ages)
-// console.log(mahsin.ages)
-// console.log(mahsagesge'](2008))
+}
