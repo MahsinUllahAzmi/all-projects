bad4dc1d9e656ecbc97ed263746d7aa0b489a349
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2024-03-23 21:20:56 +0600

    javaScript search menu filter start

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 function searchManeger(){
     const searchManeger = document.querySelector('.searchManeger')
+    const filter = searchManeger.value.toUpperCase();
     const menuList = document.querySelectorAll('.menuList')
-    menuList.forEach(item => {
-        console.log(searchManeger.indexOf(item))
-    });
+
+    menuList.forEach((item , index) => {
+        const x = item[index];
+        console.log(x)
+        if(item.innerHTML.toUpperCase().indexOf(filter) > -1){
+            item[index].style.display = '';
+        }else{
+            item[index].style.display = 'none';
+        }
+    })
 }
