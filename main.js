4b61dec1cb143a10ea55cd9f702598efca2fd00b
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2025-01-18 16:18:30 +0600

    accordion prectie 1

---
+++ main.js
@@ -0,0 +1,32 @@
+// let action_1 = document.getElementById('action_1');
+// let actionTitle = document.getElementById('actionTitle');
+// let description_1 = document.getElementById('description_1');
+// if(action_1 && actionTitle) {
+//     action_1.onclick = function (){
+//         handleAccordion()
+//     }
+//     actionTitle.onclick = function (){
+//         handleTextAccordion()
+//     }
+// }
+
+// function handleTextAccordion() {
+//     if(description_1.classList.contains('hidden')) {
+//         description_1.classList.remove('hidden')
+//         action_1.innerText = '-'
+//     }else{
+//         description_1.classList.add('hidden');
+//         action_1.innerText = '+'
+//     }
+// }
+
+
+// function handleAccordion(){
+//     if(description_1.classList.contains('hidden')) {
+//         // description_1.classList.remove('hidden');
+//         action_1.innerText = '-'
+//     }else{
+//         // description_1.classList.add('hidden');
+//         action_1.innerText = '+';
+//     }
+// }
